e13bbbd02068846301aeaad97b36c466a9cf47b5
Pritam Kumar Maurya <pritam123aur@gmail.com>
2026-08-14 17:10:42 +0530

    initial commit

---
+++ src/main/java/com/example/kafka_consumer_dedup/KafkaConsumerDedupApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafka_consumer_dedup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConsumerDedupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaConsumerDedupApplication.class, args);
+	}
+
+}
